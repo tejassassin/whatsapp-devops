22d9e9a0c08ce29268e17b8d1101358d0e6dc23f
tejassassin <tejamallela2000@gmail.com>
2024-01-05 22:50:50 +0530

    class on jan 5th

--- whatsapp-backend/controllers/user-controller.js
+++ whatsapp-backend/controllers/user-controller.js
@@ -1,6 +1,5 @@
 import Users from "../models/user.js";
 
-
 export const addUser = async (request, response) => {
   console.log(request.body);
   try {
@@ -13,6 +12,15 @@ export const addUser = async (request, response) => {
     const newUser = new Users(request.body);
     await newUser.save();
     return response.status(200).json(newUser);
+  } catch (error) {
+    response.status(500).json(error.message);
+  }
+};
+
+export const getUser = async (request, response) => {
+  try {
+    let users = await Users.find({});
+    return response.status(200).json(users);
   } catch (error) {
     response.status(500).json(error.message);
   }
--- whatsapp-backend/routes/route.js
+++ whatsapp-backend/routes/route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addUser } from "../controllers/user-controller.js";
+import { addUser, getUser } from "../controllers/user-controller.js";
 
 const route = express.Router();
 
 route.post("/add", addUser);
+route.get("/users", getUser);
 
 export default route;
